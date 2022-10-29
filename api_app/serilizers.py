d791956dc0773c637cae7e3456978f428c571946
indieka900 <indiekaj@gmail.com>
2022-10-29 23:23:27 +0300

    Django API

--- api_app/serilizers.py
+++ api_app/serilizers.py
@@ -1,12 +1,16 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 from .models import Advocate,Company
 
 
 class CompanySerilizer(ModelSerializer):
+    employee_count = SerializerMethodField(read_only=True)
     class Meta:
         model = Company
         fields = '__all__'
-        
+    
+    def get_employee_count(self, obj):
+        count = obj.advocate_set.count()
+        return count
         
 class AdvocateSerilizer(ModelSerializer):
     company = CompanySerilizer()
