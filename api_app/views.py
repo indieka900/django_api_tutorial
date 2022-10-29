3055eaa6d12fcbd76598250d436f170b4cd4271e
indieka900 <indiekaj@gmail.com>
2022-10-29 20:25:42 +0300

    Django API

--- api_app/views.py
+++ api_app/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view(['GET'])
+def endpoints(request):
+    data = ['/advocates', 'advocates/username']
+    return Response(data)
+
+def advocate_list(request):
+    data = ['Joseph', 'Ali_nyo', 'Indieka']
+    return JsonResponse(data, safe=False)
+
+def advocate_detail(request,username):
+    data = username
+    return JsonResponse(data, safe=False)
 
 # Create your views here.
